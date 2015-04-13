005c08c65cb27e7f49cdfb0fda913ed742f7a602
Julien Heurtebize <julienhtbe@gmail.com>
2015-04-13 08:31:05 +0200

    Ajout du projet générant une librairie statique à partir des sources github de laszip

---
+++ trunk/LasZipStatic/laszipstatic.cpp
@@ -0,0 +1,6 @@
+#include "laszipstatic.h"
+
+
+LasZipStatic::LasZipStatic()
+{
+}
---
+++ trunk/LasZipStatic/laszipstatic.h
@@ -0,0 +1,12 @@
+#ifndef LASZIPSTATIC_H
+#define LASZIPSTATIC_H
+
+
+class LasZipStatic
+{
+
+public:
+    LasZipStatic();
+};
+
+#endif // LASZIPSTATIC_H
