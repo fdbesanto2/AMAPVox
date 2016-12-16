7b31b96934e3aff2110e97770d499e57bdfaca0e
Julien Heurtebize <julienhtbe@gmail.com>
2016-12-16 11:05:22 +0100

    Delete a target directory

--- trunk/AMAPVox/AMAPVoxGUI/target/classes/launcher/click_me.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-freeMemory=`free | grep Mem | awk '{print $4}'`
-offset=2097152
-maxMemory=$(($freeMemory - $offset))
-echo Java max heap size fixed to ${maxMemory}k
-cd ${0%/*}
-java -jar -Xmx${maxMemory}k AMAPVoxGUI-$VERSION$-r$BUILD_NUMBER$.jar
